376e1070ffa91a0defbdd4e33027ffbe8738fd7a
Tony118g <guimaraesantony@gmail.com>
2023-05-08 14:29:46 +0000

    Add a model for photos to the photos app

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Photo(models.Model):
+    """
+    The model for photos, related to Owner/User
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    camera_used = models.CharField(
+        default='unstated',
+        max_length=100,
+        blank=True
+        )
+    lense_used = models.CharField(
+        default='unstated',
+        max_length=100,
+        blank=True
+        )
+    description = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_weccxi', blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """
+        Orders photos by date they were created
+        from newest to oldest
+        """
+        ordering = ['-created_at']
+
+    def __str__(self):
+        """
+        Returns the id and title of the photo
+        """
+        return f'{self.id} {self.title}'
