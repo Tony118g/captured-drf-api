4f5d27d1b679bc90a680073be788f25976f6acb2
Tony118g <guimaraesantony@gmail.com>
2023-05-08 15:12:56 +0000

    Add code to get and list all photos
    Add a list view with a get function in the photos app
    Link up the relevant urls

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Photo
+from .serializers import PhotoSerializer
 
-# Create your views here.
+
+class PhotoList(APIView):
+    serializer_class = PhotoSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
+    def get(self, request):
+        photos = Photo.objects.all()
+        serializer = PhotoSerializer(
+            photos, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
+
