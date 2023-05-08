cdbc659d6731c77f40d1ac07f5e6cf1b6e380bbc
Tony118g <guimaraesantony@gmail.com>
2023-05-08 14:56:33 +0000

    Add the photo serializer

---
+++ photos/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from .models import Photo
+
+
+class PhotoSerializer(serializers.ModelSerializer):
+    """
+    Provides readability for photo data in API.
+    """
+
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Photo
+        fields = [
+            'id',
+            'owner',
+            'title',
+            'camera_used',
+            'lense_used',
+            'description',
+            'image',
+            'created_at',
+            'updated_at',
+            'is_owner',
+            'profile_id',
+            'profile_image',
+        ]
