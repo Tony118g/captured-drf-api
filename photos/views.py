13231be2b9f42c2e2f2133bd81b54e0e9695debd
Tony118g <guimaraesantony@gmail.com>
2023-05-09 08:03:23 +0000

    Add functionality to delete photo posts
    Add a delete method to the PhotoDetail view to delete a photo

--- photos/views.py
+++ photos/views.py
@@ -73,3 +73,13 @@ class PhotoDetail(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        """
+        Handles the deleting of photos by id
+        """
+        photo = self.get_object(pk)
+        photo.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
