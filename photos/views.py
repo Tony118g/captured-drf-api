5adc7606fcc3abe84fac982b78d238459294d0bc
Tony118g <guimaraesantony@gmail.com>
2023-05-09 07:41:04 +0000

    Add functionality to get a photo by id
    Add a PhotoDetail view with get methods
    Link up the relevant url

--- photos/views.py
+++ photos/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Photo
 from .serializers import PhotoSerializer
+from captured_drf_api.permissions import IsOwnerOrReadOnly
 
 
 class PhotoList(APIView):
@@ -28,3 +30,31 @@ class PhotoList(APIView):
                 serializer.data, status=status.HTTP_201_CREATED
             )
         return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
+class PhotoDetail(APIView):
+    """
+    Handles retrieving, updating and deleting photos
+    """
+    serializer_class = PhotoSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_object(self, pk):
+        """
+        Gets a photo by id
+        """
+        try:
+            photo = Photo.objects.get(pk=pk)
+            self.check_object_permissions(self.request, photo)
+            return photo
+        except Photo.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+
+        photo = self.get_object(pk)
+        serializer = PhotoSerializer(
+            photo,
+            context={'request': request}
+            )
+        return Response(serializer.data)
