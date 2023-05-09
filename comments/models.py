bb23941ef2e01704f6e38de46472c5e7247406da
Tony118g <guimaraesantony@gmail.com>
2023-05-09 08:28:11 +0000

    Add a model for comments to the comments app

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from photos.models import Photo
 
-# Create your models here.
+
+class Comment(models.Model):
+    """
+    The model for comments, related to User and Photo
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """
+        Orders comments by date they were created
+        from newest to oldest
+        """
+        ordering = ['-created_at']
+
+    def __str__(self):
+        """
+        Returns the content of the comment
+        """
+        return self.content
