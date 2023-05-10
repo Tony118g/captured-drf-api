3649bc3a53a721918affe4e925e1ee7738883efe
Tony118g <guimaraesantony@gmail.com>
2023-05-10 13:16:28 +0000

    Add functionality to list and create followers
    Add a generic view with code to list and create followers
    Link up the relevant urls

--- captured_drf_api/urls.py
+++ captured_drf_api/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', include('photos.urls')),
     path('', include('comments.urls')),
     path('', include('likes.urls')),
+    path('', include('followers.urls')),
 ]
--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from rest_framework import generics, permissions
+from .models import Follower
+from .serializers import FollowerSerializer
+from captured_drf_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    Lists followers and handles creation of a follower if logged in.
+    """
+    serializer_class = FollowerSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+        ]
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
