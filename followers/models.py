bab1fcd2d617420ce066f2683c81e659abe3d1b0
Tony118g <guimaraesantony@gmail.com>
2023-05-10 13:02:10 +0000

    Add a model for followers to the followers app

--- followers/models.py
+++ followers/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Follower(models.Model):
+    """
+    The model for followers, related to User
+    """
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following'
+        )
+    followed = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='followed'
+        )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """
+        Orders followers by date they were created
+        from newest to oldest and ensures a user
+        can't follow the same user more than once
+        """
+        unique_together = ['owner', 'followed']
+        ordering = ['-created_at']
+
+    def __str__(self):
+        """
+        Returns a string with the owner and followed fields
+        """
+        return f'{self.owner} {self.followed}'
