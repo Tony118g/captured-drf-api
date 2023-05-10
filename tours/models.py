cf5bd7ffdf09db66e71be0f05d915d0ad6cc6c1c
Tony118g <guimaraesantony@gmail.com>
2023-05-10 14:01:32 +0000

    Add a model for tours to the tours app

--- tours/models.py
+++ tours/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tour(models.Model):
+    """
+    The model for tours
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    country = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    price = models.DecimalField(
+        null=True, blank=True, max_digits=4, decimal_places=2, default='00.00'
+        )
+    guide = models.CharField(
+        max_length=100,
+        blank=True,
+        default='currently unknown'
+        )
+    time_period = models.CharField(max_length=150)
+    booking_means = models.CharField(max_length=255)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_weccxi', blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """
+        Orders tours by date they were created
+        from newest to oldest
+        """
+        ordering = ['-created_at']
+
+    def __str__(self):
+        """
+        Returns the id and title of the tour
+        """
+        return f'{self.id} {self.title}'
