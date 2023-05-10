1433d8dc9aee33eab1940810eff2f57a0b0eabf5
Tony118g <guimaraesantony@gmail.com>
2023-05-10 15:19:07 +0000

    Add a model for attendances to the attendances app

--- attendances/models.py
+++ attendances/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tours.models import Tour
 
-# Create your models here.
+
+class Attendance(models.Model):
+    """
+    The model for attendances, related to User and Tour
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    tour = models.ForeignKey(
+        Tour,
+        on_delete=models.CASCADE,
+        related_name='attendances'
+        )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """
+        Orders attendances by date they were created
+        from newest to oldest and ensures a user
+        can't mark the same tour as attending twice.
+        """
+        unique_together = ['owner', 'tour']
+        ordering = ['-created_at']
+
+    def __str__(self):
+        """
+        Returns the owner and tour of the attendance
+        """
+        return f'{self.owner} {self.tour}'
