c15f2331c5ba6559b66a1c12476cf27f089772e1
Tony118g <guimaraesantony@gmail.com>
2023-05-11 11:30:31 +0000

    Add code to show follower data for profiles

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -9,10 +10,22 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return following.id if following else None
+        return None
 
     class Meta:
         model = Profile
@@ -24,5 +37,8 @@ class ProfileSerializer(serializers.ModelSerializer):
             'image',
             'created_at',
             'updated_at',
-            'is_owner'
+            'is_owner',
+            'following_id',
+            'followers_count',
+            'following_count',
         ]
