51b55383b1be106541988fcc4ffa2665dfc11bc7
Tony118g <guimaraesantony@gmail.com>
2023-05-23 17:04:24 +0000

    Add is_admin_user field to CurrentUserSerializer

--- captured_drf_api/serializers.py
+++ captured_drf_api/serializers.py
@@ -5,8 +5,15 @@ from rest_framework import serializers
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
+    is_admin_user = serializers.SerializerMethodField()
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
-            'profile_id', 'profile_image'
+            'profile_id', 'profile_image', 'is_admin_user'
         )
+
+    def get_is_admin_user(self, obj):
+        """
+        Returns true if the user is a admin/staff user
+        """
+        return obj.is_staff
