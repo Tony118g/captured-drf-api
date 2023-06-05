b20427c7a8500a5ea8b923fa0a8e1c7a914649d4
Tony118g <guimaraesantony@gmail.com>
2023-06-05 21:28:07 +0000

    Add automated tests to comments and followers apps

--- comments/tests.py
+++ comments/tests.py
@@ -1,3 +1,158 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Comment
+from photos.models import Photo
 
-# Create your tests here.
+
+class CommentListViewTests(APITestCase):
+    """
+    Tests the list view for the comments app.
+    """
+    def setUp(self):
+        """
+        Sets up a user and photo to be used in the tests.
+        """
+        self.user1 = User.objects.create_user(
+            username='user1', password='password1'
+            )
+        self.photo1 = Photo.objects.create(
+            owner=self.user1,
+            title='title 1',
+            )
+
+    def test_can_list_comments(self):
+        """
+        Tests if all comments can be fetched and listed.
+        """
+        Comment.objects.create(
+            owner=self.user1, content='comment 1', photo=self.photo1
+            )
+        response = self.client.get('/comments/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logged_in_user_can_create_comment(self):
+        """
+        Tests if logged in users can create a comment.
+        """
+        self.client.force_login(self.user1)
+        response = self.client.post('/comments/', {
+            'content': 'comment 1',
+            'photo': 1,
+            })
+        self.assertEqual(Comment.objects.all().count(), 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_not_logged_in_user_cannot_create_comment(self):
+        """
+        Tests if logged out users cannot create a comment.
+        """
+        response = self.client.post('/comments/', {
+            'content': 'comment 1',
+            'photo': 1,
+            })
+        self.assertEqual(Comment.objects.all().count(), 0)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class CommentDetailViewTests(APITestCase):
+    """
+    Tests the detail view for the comments app.
+    """
+    def setUp(self):
+        """
+        Sets up users, photo and comments to be used in the tests.
+        """
+        self.user1 = User.objects.create_user(
+            username='user1', password='password1'
+            )
+        self.user2 = User.objects.create_user(
+            username='user2', password='password2'
+            )
+        self.photo1 = Photo.objects.create(
+            owner=self.user1,
+            title='title 1',
+            )
+        Comment.objects.create(
+            owner=self.user1, photo=self.photo1
+            )
+        Comment.objects.create(
+            owner=self.user2, photo=self.photo1
+            )
+
+    def test_can_retrieve_comment_by_valid_id(self):
+        """
+        Tests if a commment with a valid id can be fetched and viewed.
+        """
+        response = self.client.get('/comments/1/')
+        self.assertEqual(response.data['photo'], 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cannot_retrieve_comment_by_invalid_id(self):
+        """
+        Tests if it is possible to fetch and view a
+        comment with an invalid id.
+        """
+        response = self.client.get('/comments/4007/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_own_comment(self):
+        """
+        Tests if a user can update their own comment.
+        """
+        self.client.force_login(self.user1)
+        response = self.client.put(
+            '/comments/1/', {'content': 'edited comment'}
+            )
+        comment = Comment.objects.filter(pk=1).first()
+        self.assertEqual(comment.content, 'edited comment')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cannot_update_another_users_comment(self):
+        """
+        Tests if a user cannot edit another user's comment.
+        """
+        self.client.force_login(self.user1)
+        response = self.client.put(
+            '/comments/2/', {'content': 'edited comment'}
+            )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_can_delete_own_photo_comment(self):
+        """
+        Tests if a user can delete their own comment.
+        """
+        self.client.force_login(self.user1)
+        response = self.client.delete('/comments/1/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Comment.objects.all().count(), 1)
+
+    def test_user_cannot_delete_another_users_comment(self):
+        """
+        Tests if a user can delete another user's comment.
+        """
+        self.client.force_login(self.user1)
+        response = self.client.delete('/comments/2/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(Comment.objects.all().count(), 2)
+
+
+class CommentModelTests(APITestCase):
+    """
+    Tests the comment model.
+    """
+    def test_model_returns_comment_content(self):
+        """
+        Tests if the comment content is returned from the model.
+        """
+        user = User.objects.create_user(
+            username='user1', password='password1'
+            )
+        photo = Photo.objects.create(
+            owner=user,
+            title='title 1',
+            )
+        Comment.objects.create(
+            owner=user, photo=photo, content='comment content'
+            )
+        self.assertEquals(str(Comment.objects.first()), "comment content")
--- followers/tests.py
+++ followers/tests.py
@@ -1,3 +1,129 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Follower
 
-# Create your tests here.
+
+class FollowerListViewTests(APITestCase):
+    """
+    Tests the list view for the followers app.
+    """
+    def setUp(self):
+        """
+        Sets up users to be used in the tests.
+        """
+        self.user1 = User.objects.create_user(
+            username='user1', password='password1'
+            )
+        self.user2 = User.objects.create_user(
+            username='user2', password='password2'
+            )
+
+    def test_can_list_followers(self):
+        """
+        Tests if all followers can be fetched and listed.
+        """
+        Follower.objects.create(
+            owner=self.user1, followed=self.user2
+            )
+        response = self.client.get('/followers/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logged_in_user_can_follow_a_user(self):
+        """
+        Tests if logged in users can follow other users.
+        """
+        self.client.force_login(self.user1)
+        response = self.client.post('/followers/', {
+            'followed': 2,
+            })
+        self.assertEqual(Follower.objects.all().count(), 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_not_logged_in_user_cannot_follow_a_user(self):
+        """
+        Tests if logged out users cannot follow another user.
+        """
+        response = self.client.post('/followers/', {
+            'followed': 2,
+            })
+        self.assertEqual(Follower.objects.all().count(), 0)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class FollowerDetailViewTests(APITestCase):
+    """
+    Tests the detail view for the followers app.
+    """
+    def setUp(self):
+        """
+        Sets up users and followers to be used in the tests.
+        """
+        self.user1 = User.objects.create_user(
+            username='user1', password='password1'
+            )
+        self.user2 = User.objects.create_user(
+            username='user2', password='password2'
+            )
+        self.user3 = User.objects.create_user(
+            username='user3', password='password3'
+            )
+
+        Follower.objects.create(owner=self.user1, followed=self.user2)
+        Follower.objects.create(owner=self.user2, followed=self.user3)
+
+    def test_can_retrieve_follower_by_valid_id(self):
+        """
+        Tests if a follower with a valid id can be fetched and viewed.
+        """
+        response = self.client.get('/followers/1/')
+        self.assertEqual(response.data['followed'], 2)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cannot_retrieve_follower_by_invalid_id(self):
+        """
+        Tests if it is possible to fetch and view a
+        follower with an invalid id.
+        """
+        response = self.client.get('/followers/4007/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_unfollow_a_user(self):
+        """
+        Tests if a user can unfollow another user.
+        """
+        self.client.force_login(self.user1)
+        response = self.client.delete('/followers/1/')
+        self.assertEqual(Follower.objects.all().count(), 1)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_user_cannot_unfollow_on_another_users_behalf(self):
+        """
+        Tests if a user cannot unfollow users on another user's behalf.
+        """
+        self.client.force_login(self.user1)
+        response = self.client.delete('/followers/2/')
+        self.assertEqual(Follower.objects.all().count(), 2)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class FollowerModelTests(APITestCase):
+    """
+    Tests the follower model
+    """
+    def test_model_returns_owner_and_followed_string(self):
+        """
+        Tests if the correct string with the follower owner and followed field
+        is returned from the model.
+        """
+        user1 = User.objects.create_user(
+            username='user1', password='password1'
+            )
+        user2 = User.objects.create_user(
+            username='user2', password='password2'
+            )
+
+        Follower.objects.create(
+            owner=user1, followed=user2
+            )
+        self.assertEquals(str(Follower.objects.first()), "user1 user2")
