b77b010d7608464bd3aa5e73d0c80f508706a472
Tony118g <guimaraesantony@gmail.com>
2023-06-05 21:48:24 +0000

    Add automated tests for tours and attendances apps

--- attendances/tests.py
+++ attendances/tests.py
@@ -1,3 +1,150 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Attendance
+from tours.models import Tour
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class AttendanceListViewTests(APITestCase):
+    """
+    Tests the list view for the attendance app.
+    """
+    def setUp(self):
+        """
+        Sets up a user and tour to be used in the tests.
+        """
+        self.user1 = User.objects.create_user(
+            username='user1', password='password1'
+            )
+        self.tour1 = Tour.objects.create(
+            title='title 1',
+            owner=self.user1,
+            start_date='2024-11-11',
+            end_date='2024-12-12',
+            booking_means='book online',
+            country='England',
+            city='London'
+            )
+
+    def test_can_list_attendances(self):
+        """
+        Tests if all attendances can be fetched and listed.
+        """
+        Attendance.objects.create(
+            owner=self.user1, tour=self.tour1
+            )
+        response = self.client.get('/attendances/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logged_in_user_can_mark_attendance(self):
+        """
+        Tests if logged in users can mark tour attendance.
+        """
+        self.client.force_login(self.user1)
+        response = self.client.post('/attendances/', {
+            'tour': 1,
+            })
+        self.assertEqual(Attendance.objects.all().count(), 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_not_logged_in_user_cannot_mark_attendance(self):
+        """
+        Tests if logged out users cannot mark tour attendance.
+        """
+        response = self.client.post('/attendances/', {
+            'tour': 1,
+            })
+        self.assertEqual(Attendance.objects.all().count(), 0)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class AttendanceDetailViewTests(APITestCase):
+    """
+    Tests the detail view for the attendance app.
+    """
+    def setUp(self):
+        """
+        Sets up users, tours and attendances to be used in the tests.
+        """
+        self.user1 = User.objects.create_user(
+            username='user1', password='password1'
+            )
+        self.user2 = User.objects.create_user(
+            username='user2', password='password2'
+            )
+        self.tour = Tour.objects.create(
+            title='title 1',
+            owner=self.user1,
+            start_date='2024-11-11',
+            end_date='2024-12-12',
+            booking_means='book online',
+            country='England',
+            city='London'
+            )
+        Attendance.objects.create(
+            owner=self.user1, tour=self.tour
+            )
+        Attendance.objects.create(
+            owner=self.user2, tour=self.tour
+            )
+
+    def test_can_retrieve_attendance_by_valid_id(self):
+        """
+        Tests if an attendance with a valid id can be fetched and viewed.
+        """
+        response = self.client.get('/attendances/1/')
+        self.assertEqual(response.data['tour'], 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cannot_retrieve_attendance_by_invalid_id(self):
+        """
+        Tests if it is possible to fetch and view an
+        attendance with an invalid id.
+        """
+        response = self.client.get('/attendances/4007/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_unmark_own_attendance(self):
+        """
+        Tests if a user can unmark their own attendance on a tour.
+        """
+        self.client.force_login(self.user1)
+        response = self.client.delete('/attendances/1/')
+        self.assertEqual(Attendance.objects.all().count(), 1)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_user_cannot_unmark_another_users_attendance(self):
+        """
+        Tests if a user cannot unmark another user's attendance on a tour.
+        """
+        self.client.force_login(self.user1)
+        response = self.client.delete('/attendances/2/')
+        self.assertEqual(Attendance.objects.all().count(), 2)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class AttendanceModelTests(APITestCase):
+    """
+    Tests the attendance model
+    """
+    def test_model_returns_owner_and_tour_string(self):
+        """
+        Tests if the correct string with the attendance owner and tour
+        is returned from the model.
+        """
+        user = User.objects.create_user(
+            username='user', password='password1'
+            )
+        tour = Tour.objects.create(
+            title='title 1',
+            owner=user,
+            start_date='2024-11-11',
+            end_date='2024-12-12',
+            booking_means='book online',
+            country='England',
+            city='London'
+            )
+        Attendance.objects.create(
+            owner=user, tour=tour
+            )
+        self.assertEquals(str(Attendance.objects.first()), "user 1 title 1")
--- tours/tests.py
+++ tours/tests.py
@@ -1,3 +1,179 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Tour
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class TourListViewTests(APITestCase):
+    """
+    Tests the list view for the tours app.
+    """
+    def setUp(self):
+        """
+        Sets up users to be used in the tests.
+        """
+        self.user1 = User.objects.create_user(
+            username='user1', password='password1'
+            )
+        self.staff_user = User.objects.create_user(
+            username="staff",
+            password="staff_password",
+            is_staff=True
+        )
+
+    def test_can_list_tours(self):
+        """
+        Tests all tours can be fetched and listed.
+        """
+        Tour.objects.create(
+            title='title 1',
+            owner=self.user1,
+            start_date='2024-11-11',
+            end_date='2024-12-12',
+            booking_means='book online',
+            country='England',
+            city='London'
+            )
+        response = self.client.get('/tours/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_staff_user_can_create_tour(self):
+        """
+        Tests if a logged in staff user can create a tour.
+        """
+        self.client.force_login(self.staff_user)
+        response = self.client.post('/tours/', {
+            'title': 'test title',
+            'start_date': '2024-11-11',
+            'end_date': '2024-12-12',
+            'booking_means': 'book online',
+            'country': 'England',
+            'city': 'London'
+            })
+        self.assertEqual(Tour.objects.all().count(), 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_non_staff_user_cannot_create_tour(self):
+        """
+        Tests if a non staff user cannot create a tour.
+        """
+        self.client.force_login(self.user1)
+        response = self.client.post('/tours/', {
+            'title': 'test title',
+            'start_date': '2024-11-11',
+            'end_date': '2024-12-12',
+            'booking_means': 'book online',
+            'country': 'England',
+            'city': 'London'
+            })
+        self.assertEqual(Tour.objects.all().count(), 0)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class TourDetailViewTests(APITestCase):
+    """
+    Tests the detail view for the tours app.
+    """
+    def setUp(self):
+        """
+        Sets up users and a tour to be used in the tests.
+        """
+        self.user1 = User.objects.create_user(
+            username='user1', password='password1'
+            )
+        self.staff_user = User.objects.create_user(
+            username="staff",
+            password="staff_password",
+            is_staff=True
+        )
+        Tour.objects.create(
+            title='title 1',
+            owner=self.user1,
+            start_date='2024-11-11',
+            end_date='2024-12-12',
+            booking_means='book online',
+            country='England',
+            city='London'
+            )
+
+    def test_can_retrieve_tour_by_valid_id(self):
+        """
+        Tests if a tour with a valid id can be fetched and viewed.
+        """
+        response = self.client.get('/tours/1/')
+        self.assertEqual(response.data['title'], 'title 1')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cannot_retrieve_tour_by_invalid_id(self):
+        """
+        Tests if it is possible to fetch and view a tour with an invalid id.
+        """
+        response = self.client.get('/tours/4007/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_staff_user_can_update_tour(self):
+        """
+        Tests if a staff user can update a tour.
+        """
+        self.client.force_login(self.staff_user)
+        response = self.client.put('/tours/1/', {
+            'title': 'edited title',
+            'start_date': '2024-11-11',
+            'end_date': '2024-12-12',
+            'booking_means': 'book online',
+            'country': 'England',
+            'city': 'London'
+            })
+        tour = Tour.objects.filter(pk=1).first()
+        self.assertEqual(tour.title, 'edited title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_non_staff_user_cannot_update_tour(self):
+        """
+        Tests if a non staff user cannot edit a tour.
+        """
+        self.client.force_login(self.user1)
+        response = self.client.put('/tours/1/', {'title': 'edited title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_staff_user_can_delete_tour(self):
+        """
+        Tests if a staff user can delete a tour.
+        """
+        self.client.force_login(self.staff_user)
+        response = self.client.delete('/tours/1/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Tour.objects.all().count(), 0)
+
+    def test_non_staff_user_cannot_delete_tour(self):
+        """
+        Tests if a non staff user cannot delete a tour.
+        """
+        self.client.force_login(self.user1)
+        response = self.client.delete('/tours/1/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(Tour.objects.all().count(), 1)
+
+
+class TourModelTests(APITestCase):
+    """
+    Tests the tour model.
+    """
+    def test_model_returns_id_and_title_string(self):
+        """
+        Tests if the correct string with the tour id and title
+        is returned from the model.
+        """
+        user1 = User.objects.create_user(
+            username='user1', password='password1'
+            )
+        Tour.objects.create(
+            title='title 1',
+            owner=user1,
+            start_date='2024-11-11',
+            end_date='2024-12-12',
+            booking_means='book online',
+            country='England',
+            city='London'
+            )
+        self.assertEquals(str(Tour.objects.first()), "1 title 1")
